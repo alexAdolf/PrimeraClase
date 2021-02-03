b5eea881ae130af6267113b884983950d73db7cd
adolfo astorga <adolfo.astorga@clave.cl>
2021-02-02 22:57:12 -0300

    [CHG] Primero cambios de Skin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Com1Component } from './components/com1/com1.component';
+import { Com2Component } from './components/com2/com2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Com1Component,
+    Com2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
